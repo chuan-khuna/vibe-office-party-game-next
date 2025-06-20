2056c3e76a468eb65a7ae31dd883d456036ad263
ALTR <chuan.khuna@gmail.com>
2025-06-20 18:07:30 +0700

    feat: add @vercel/analytics integration and update next.config.ts for typescript settings

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  // ignoreBuildErrors: true,
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 export default nextConfig;
